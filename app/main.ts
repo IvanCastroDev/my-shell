2fb0d5df34e8457314df5a80c7656cfcfb79e1c1
Ivan Castro <ivancastromartinezkk@gmail.com>
2025-01-29 20:30:38 -0600

    type function implementation

--- app/main.ts
+++ app/main.ts
@@ -1,42 +1,52 @@
 import { exit } from "process";
 import { createInterface } from "readline";
 
+// Types
+type CommandFunction = (args: string[]) => void;
+
+// Global variables
 const rl = createInterface({
   input: process.stdin,
   output: process.stdout,
 });
 
-const exitFunction = (reject: (reason?: any) => void, code: string) => {
-  rl.close();
-  reject(code)
-};
-
-const echo = (message: string) => {
-  console.log(message);
+// Functions for the shell commands
+const echo = (args: string[]) => {
+  console.log(args.join(' '));
 }
 
-const commands = {
-  'exit': exitFunction,
-  'echo': echo
+const typeFunction = (args: string[]) => {
+  if (commands[args.join(' ')] !== undefined) {
+    console.log(`${args} is a shell builtin`);
+  } else {
+    console.log(`${args.join(' ')} not found`);
+  }
+}
+
+const commands: { [key: string]: CommandFunction } = {
+  'echo': echo,
+  'type': typeFunction
 }
 
 const readUserInput = async () => {
   return new Promise<void>((resolve, reject) => {
     rl.question("$ ", (answer) => {
       const command = answer.split(" ")[0];
-      const args = answer.split(" ").length > 1 ? answer.split(" ").slice(1, answer.length) : [''];
+      const args = answer.split(" ").length > 1 ? answer.split(" ").slice(1, answer.length) : [];
 
-      switch (command) {
-        case 'exit':
-          commands[command](reject, args[1]);
-          break;
-        case 'echo':
-          commands[command](args.join(" "));
-          break;
-        default:
-          console.log(`${command}: command not found`);
-          break;
-      }
+      if (command === 'exit') {
+        rl.close();
+        reject(args.length > 0 ? parseInt(args.join(' ')) : 0);
+        return;
+      };
+
+      if (!commands[command]) {
+        console.log(`${command}: command not found`);
+        resolve();
+        return;
+      };
+
+      commands[command](args);
 
       resolve();
     });
@@ -46,7 +56,7 @@ const readUserInput = async () => {
 while (true) {
   try {
     await readUserInput();
-  } catch (reason) {
-    throw exit(0)
+  } catch (code: any) {
+    throw exit(code);
   }
 }
