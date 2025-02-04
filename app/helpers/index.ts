2814449fcd0116e5a93d9b9587d246e18ce8e46e
Ivan Castro <ivancastromartinezkk@gmail.com>
2025-02-04 14:45:57 -0600

    update

--- app/helpers/index.ts
+++ app/helpers/index.ts
@@ -1,6 +1,12 @@
 import { execSync } from 'child_process';
 import { OS } from '../constants';
 
+export const formatCommand = (answer: string): string[] => {
+  const [command, ...args] = [...answer.matchAll(/"([^"]*)"|'([^']*)'|(\S+)/g)].map(m => m[1] || m[2] || m[3]);
+
+  return [command, ...args];
+};
+
 export const commandExists = (command: string): string | void => {
     let separator = OS === 'win32' ? ';' : ':';
     let PATH = process.env.PATH || '';
--- app/main.ts
+++ app/main.ts
@@ -1,14 +1,14 @@
 import { rl } from "./constants";
 import commands from "./commands";
 import { commandExists, execInternalCommand } from "./helpers";
-import { singleQuoteRegex } from "./constants";
+import { formatCommand } from "./helpers";
 
 // Main loop
 const main = () => {
   return new Promise<void>((resolve) => {
     rl.question("$ ", async (answer) => {
-      const [command, ...args] = [...answer.matchAll(/"([^"]*)"|'([^']*)'|(\S+)/g)].map(m => m[1] || m[2] || m[3]);
-  
+      const [command, ...args] = formatCommand(answer);
+
       if (commands[command]) {
         commands[command](args);
         resolve();
